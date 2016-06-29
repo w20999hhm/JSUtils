e2daa876f6f7d54e7dac0850e83d28dda1c371c5
w20999hhm <w20999hf@qq.com>
2016-06-29 09:30:16 +0800

    add

--- utils.js
+++ utils.js
@@ -1,10 +1,10 @@
 /**
  * Created by WK on 2016/3/2.
  */
-(function (utils) {
+(function (u) {
 
     //获取URL参数
-    utils.getParame = function (name) {
+    u.getParame = function (name) {
         var code = '';
         var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
         var r = window.location.search.substr(1).match(reg);
@@ -17,7 +17,7 @@
     }
 
     //转化时间格式
-    utils.getTime = function (input) {
+    u.getTime = function (input) {
         if (input) {
             var date = new Date(input);
             var seperator1 = "-";
@@ -37,5 +37,33 @@
             return '';
         }
     }
-})
-(window.utils = {})
+	
+	 //验证手机号码是否合法
+    u.isMobile = function (value) {
+        var mobile = /^(((13[0-9]{1})|(15[0-9]{1})|(14[1-9]{1})|(17[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
+        return mobile.test(value)
+    }
+
+    // 验证密码是否复合安全要求
+    u.validPassword = function (value) {
+        var regex = /^(?![^a-zA-Z]+$)(?!\D+$).{6,18}$/;
+        return regex.test(value);
+    }
+	
+	//URL参数编码
+	u.urlParamsEncode = function (param, key, encode) {
+        if (param == null) return '';
+        var paramStr = '';
+        var t = typeof (param);
+        if (t == 'string' || t == 'number' || t == 'boolean') {
+            paramStr += '&' + key + '=' + ((encode == null || encode) ? encodeURIComponent(param) : param);
+        } else {
+            for (var i in param) {
+                var k = key == null ? i : key + (param instanceof Array ? '[' + i + ']' : '.' + i);
+                paramStr += u.urlParamsEncode(param[i], k, encode);
+            }
+        }
+        return paramStr;
+    };
+
+})(window.utils = {})
